f16157233a4112b5e629ab4e9f96694bbf6a8b87
Lalit Sharma <sharma.lalitm@gmail.com>
2019-03-22 16:55:56 +0530

    Projet structure

---
+++ NABTest/DepositBusiness/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace DepositBusiness
+{
+    public class Class1
+    {
+    }
+}
---
+++ NABTest/NABTest/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="NABTest.Global" Language="C#" %>
